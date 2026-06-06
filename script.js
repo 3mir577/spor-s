0a315f9707c6546afdd9561be4e53f4d0de835ce
Emir <emirunluturk2008@gmail.com>
2026-06-06 13:01:59 +0300

    tasarim guncelleme

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
+let veriler = JSON.parse(localStorage.getItem("veriler")) || [];
+
 function kaydet() {
-    let kilo = document.getElementById("kilo").value;
-    document.getElementById("sonuc").innerText =
-        "Bugünkü kilon: " + kilo;
+    localStorage.setItem("veriler", JSON.stringify(veriler));
+    goster();
 }
+
+function kiloEkle() {
+    let kilo = document.getElementById("kilo").value;
+
+    veriler.push({
+        tip: "kilo",
+        veri: kilo + " kg",
+        tarih: new Date().toLocaleDateString()
+    });
+
+    kaydet();
+}
+
+function agirlikEkle() {
+    let hareket = document.getElementById("hareket").value;
+    let agirlik = document.getElementById("agirlik").value;
+
+    veriler.push({
+        tip: "ağırlık",
+        veri: hareket + " - " + agirlik + " kg",
+        tarih: new Date().toLocaleDateString()
+    });
+
+    kaydet();
+}
+
+function goster() {
+    let liste = document.getElementById("liste");
+    liste.innerHTML = "";
+
+    veriler.forEach(v => {
+        liste.innerHTML += `
+            <p>${v.tarih} | ${v.tip.toUpperCase()} | ${v.veri}</p>
+        `;
+    });
+}
+
+goster();
